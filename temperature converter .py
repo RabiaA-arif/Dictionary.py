682b2b84d2b3a955adbb7eda1d5c9af26ff148a5
RabiaArif <151766471+RabiaA-arif@users.noreply.github.com>
2024-02-24 21:22:46 +0500

    Add files via upload

---
+++ temperature converter .py
@@ -0,0 +1,12 @@
+conversion_factor={}
+
+#F = (C * 9/5) + 32
+#C = (F - 32) * 5/9
+celsius=35
+fareheight= celsius * 9/5 + 32
+conversion_factor[fareheight]=celsius
+print(conversion_factor)
+far=56
+cel=(far-32) * 5/9
+conversion_factor[cel]=far
+print(conversion_factor)
