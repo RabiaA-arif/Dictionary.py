f37601425431c67db623da1c8d41f2175afbc51b
RabiaArif <151766471+RabiaA-arif@users.noreply.github.com>
2024-02-24 21:18:29 +0500

    Add files via upload

---
+++ shopping_list_manger.py
@@ -0,0 +1,9 @@
+# Shopping List Manager:
+# Develop a program that allows users to add, remove, and check off items on a shopping list 
+#stored in a dictionary
+
+shopping_items={"items":["fruits","vegetables","mangoes"]}
+shopping_items["items"].append(999)
+print(shopping_items)
+shopping_items['items'].remove("fruits")
+print(shopping_items)
